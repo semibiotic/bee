05022ef19570336541d9d3c663e74e2fb2c2756a
shadow <shadow@71f00a00-e368-4fde-a356-73d88c167424>
2002-06-01 18:57:53 +0000

    bugfix: TAB is delimiter also
    git-svn-id: file:///svn/bee@40 71f00a00-e368-4fde-a356-73d88c167424

--- modules/ipstatd-traff/beetraff.h
+++ modules/ipstatd-traff/beetraff.h
@@ -1,9 +1,9 @@
-/* $RuOBSD: beetraff.h,v 1.2 2001/09/12 05:03:21 tm Exp $ */
+/* $RuOBSD: beetraff.h,v 1.1 2002/04/01 02:36:42 shadow Exp $ */
 
 #ifndef __BEETRAFF_H__
 #define __BEETRAFF_H__
 
 #define MAX_STRLEN 1024
-#define IPFSTAT_DELIM " \n"
+#define IPFSTAT_DELIM " \t\n"
 
 #endif /* __BEETRAFF_H__ */
