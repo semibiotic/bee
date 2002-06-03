71c23bb129cb60f33030c10427d86fc95da82bce
shadow <shadow@71f00a00-e368-4fde-a356-73d88c167424>
2002-06-03 17:38:50 +0000

    sync
    git-svn-id: file:///svn/bee@50 71f00a00-e368-4fde-a356-73d88c167424

--- modules/scripts/beeapply.sh
+++ modules/scripts/beeapply.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-# $RuOBSD$
+# $RuOBSD: beeapply.sh,v 1.2 2001/09/12 05:03:21 tm Exp $
 
-# IPF rules set-up
-ipf -Fa -f /var/bee/ipf.rules.effective
+# IPF rules set-up (local)
+#ipf -Fa -f /var/bee/ipf.rules.effective
+
+# PF rules set-up (local)
+#pfctl -R /var/bee/pf.conf.effective 
