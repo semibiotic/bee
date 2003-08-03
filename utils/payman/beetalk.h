31d8a3825808175b0926c89308889609ec569291
shadow <shadow@71f00a00-e368-4fde-a356-73d88c167424>
2003-08-03 16:46:33 +0000

    sync
    git-svn-id: file:///svn/bee@107 71f00a00-e368-4fde-a356-73d88c167424

--- utils/payman/beetalk.h
+++ utils/payman/beetalk.h
@@ -3,14 +3,13 @@
 
 #include <stdarg.h>
 
-
 // Connect bee & initialize session
 int bee_enter();
 
 // Send command
 int bee_send    (char * cmd, char * argfmt, ...);
 // Receive answer
-int bee_recv    (int code, char * buf, int sz, char ** msg, int * last);
+int bee_recv    (int code, char ** pmsg, int * last);
 
 // Send/Recv "update" command
 int bee_update     ();
