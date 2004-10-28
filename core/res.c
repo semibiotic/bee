9e5e9a91d3819e4a765c087f6286258922e3a4aa
shadow <shadow@71f00a00-e368-4fde-a356-73d88c167424>
2004-10-28 01:54:21 +0000

    new price-by-time code
    git-svn-id: file:///svn/bee@167 71f00a00-e368-4fde-a356-73d88c167424

--- core/res.c
+++ core/res.c
@@ -1,4 +1,4 @@
-/* $RuOBSD: res.c,v 1.6 2003/07/28 03:19:46 shadow Exp $ */
+/* $RuOBSD: res.c,v 1.7 2004/04/20 04:02:17 shadow Exp $ */
 
 #include <stdio.h>
 #include <syslog.h>
@@ -20,15 +20,52 @@ resource_t  resource[]=
 
 #define DELIM  " ,\t\n\r"
 
+typedef struct
+{  int      hour_from;
+   int      hour_to;
+   money_t  price;
+} inet_tariff_t;
+
+inet_tariff_t  inet_tariffs[] =
+{  { 0,  0,  3.0},  // default price
+   { 2,  9,  1.8},  // night dead time
+   {15, 19,  3.5},  // day rush hour
+   {-1, -1, -1  }   // (terminator)
+};
+
 money_t inet_count_proc(is_data_t * data)
-{
-#define INET_1M_PRICE 3
+{  money_t    retval = 0;
+   time_t     curtime;
+   time_t     stime;
+   struct tm  stm;
+   int        tariff = 0;  // default tariff
+   int        i;
 
-// (Uncomment for free Outbound traffic)
-//   if ((data->proto_id & 0x80000000) != 0) return 0; 
+#define CORR_VALUE           300      /* 5 minutes */
 
-// Inbound traffic
-     return -((money_t)data->value * INET_1M_PRICE /(1024*1024));   
+// Get current time
+   curtime = time(NULL);
+   localtime_r(&curtime, &stm);
+
+// Count day start time (0:00)
+   stm = stm;
+   stm.tm_hour = 0;
+   stm.tm_min  = 0;
+   stm.tm_sec  = 0;   
+   stime  = timelocal(&stm);
+
+// Add realtime time correction
+   stime += CORR_VALUE;  
+
+// Find tariff index
+   for (i=1; inet_tariffs[i].hour_from >= 0; i++)
+   {  if (curtime >= (stime + inet_tariffs[i].hour_from * 3600) &&
+          curtime <  (stime + inet_tariffs[i].hour_to   * 3600))  tariff = i;
+   }
+   
+   retval = -((money_t)data->value * inet_tariffs[tariff].price /(1024*1024));
+
+   return retval;
 }
 
 money_t mail_count_proc(is_data_t * data)
