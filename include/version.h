e2fe106c498005d06681c90b6bbd2702377ca4cb
shadow <shadow@71f00a00-e368-4fde-a356-73d88c167424>
2006-12-30 09:33:09 +0000

    implement true version control
    git-svn-id: file:///svn/bee@219 71f00a00-e368-4fde-a356-73d88c167424

---
+++ include/version.h
@@ -0,0 +1,11 @@
+/* $RuOBSD$ */
+#ifndef __VERSION_H__
+#define __VERSION_H__
+
+#define VER_VER     0
+#define VER_SUBVER  5
+#define VER_REV     0
+#define VER_SUBREV  0
+
+#endif /*__VERSION_H__*/
+
