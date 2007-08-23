f81b2a630a5990cc931b22086110ba09f7b1a6c1
shadow <shadow@71f00a00-e368-4fde-a356-73d88c167424>
2007-08-23 07:40:32 +0000

    + resource :-\
    git-svn-id: file:///svn/bee@230 71f00a00-e368-4fde-a356-73d88c167424

--- modules/traff/res.c
+++ modules/traff/res.c
@@ -1,4 +1,4 @@
-/* $RuOBSD: res.c,v 1.1 2004/05/08 15:35:55 shadow Exp $ */
+/* $RuOBSD: res.c,v 1.1 2005/07/30 22:43:13 shadow Exp $ */
 
 #include <stdio.h>
 #include <syslog.h>
@@ -9,12 +9,13 @@
 #include <bee.h>
 #include <res.h>
 
-int         resourcecnt=4;
+int         resourcecnt=5;
 resource_t  resource[]=
 {  
    {0, NULL,  "inet",   NULL, 1},
    {0, NULL,  "mail",   NULL, 0},
    {0, NULL, "adder",   NULL, 0},
    {0, NULL, "intra",   NULL, 0},
+   {0, NULL,"charge",   NULL, 0}
 };
 
