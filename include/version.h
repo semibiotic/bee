0450ab7deb34b456138d4ad6c0712f0a41042d9f
shadow <shadow@71f00a00-e368-4fde-a356-73d88c167424>
2007-08-28 02:08:34 +0000

    inc subversion
    git-svn-id: file:///svn/bee@234 71f00a00-e368-4fde-a356-73d88c167424

--- include/version.h
+++ include/version.h
@@ -1,11 +1,11 @@
-/* $RuOBSD: version.h,v 1.4 2007/08/22 09:28:54 shadow Exp $ */
+/* $RuOBSD: version.h,v 1.5 2007/08/23 08:43:46 shadow Exp $ */
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
 #define VER_VER     0
-#define VER_SUBVER  6
+#define VER_SUBVER  7
 #define VER_REV     0
-#define VER_SUBREV  1
+#define VER_SUBREV  0
 
 #endif /*__VERSION_H__*/
 
