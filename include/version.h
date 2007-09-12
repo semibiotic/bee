6b45f0e1e84fa3cf1f6a5f16f5b3640f77c31da9
shadow <shadow@71f00a00-e368-4fde-a356-73d88c167424>
2007-09-12 08:57:31 +0000

    rev up (accsumm tests)
    git-svn-id: file:///svn/bee@245 71f00a00-e368-4fde-a356-73d88c167424

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
-/* $RuOBSD: version.h,v 1.7 2007/09/07 02:27:28 shadow Exp $ */
+/* $RuOBSD: version.h,v 1.8 2007/09/10 04:47:58 shadow Exp $ */
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
 #define VER_VER     0
 #define VER_SUBVER  7
-#define VER_REV     3
+#define VER_REV     4
 #define VER_SUBREV  0
 
 #endif /*__VERSION_H__*/
