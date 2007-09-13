46838ab832ec5944c6c30a479b7898d70717fdde
shadow <shadow@71f00a00-e368-4fde-a356-73d88c167424>
2007-09-13 10:30:52 +0000

    fix: update script (dump format options) to current parser version
    git-svn-id: file:///svn/bee@250 71f00a00-e368-4fde-a356-73d88c167424

--- modules/scripts/bee-cnupmdump.sh
+++ modules/scripts/bee-cnupmdump.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $RuOBSD$
+# $RuOBSD: bee-cnupmdump.sh,v 1.1 2005/07/30 22:43:13 shadow Exp $
 
 IFACE="pflog0"
 
@@ -10,7 +10,7 @@ pkill -HUP cnupm
 sleep 2
 
 # output text dump for bee
-/usr/local/sbin/cnupmstat -BEn ${IFACE}
+/usr/local/sbin/cnupmstat -En ${IFACE}
 
 # remove dumpfile
 rm -rf /var/cnupm/cnupm-${IFACE}.dump
