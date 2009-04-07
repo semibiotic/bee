2987588fdebc5d6040fac403326cec6cc7b96577
shadow <shadow@71f00a00-e368-4fde-a356-73d88c167424>
2009-04-07 09:24:10 +0000

    rev up (unlimits logging)
    git-svn-id: file:///svn/bee@364 71f00a00-e368-4fde-a356-73d88c167424

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
-/* $RuOBSD: version.h,v 1.28 2008/12/26 05:25:20 shadow Exp $ */
+/* $RuOBSD: version.h,v 1.29 2009/04/07 03:06:52 shadow Exp $ */
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
 #define VER_VER     0
 #define VER_SUBVER  9
-#define VER_REV     9
+#define VER_REV     10
 #define VER_SUBREV  0
 
 #endif /*__VERSION_H__*/
