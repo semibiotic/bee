83dd666fc7d072f1298990cf847dde9e06eee291
shadow <shadow@71f00a00-e368-4fde-a356-73d88c167424>
2009-04-07 03:06:52 +0000

    force use index file, obsolete -d switch clean-up usage (remove -I and -d)
    git-svn-id: file:///svn/bee@358 71f00a00-e368-4fde-a356-73d88c167424

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
-/* $RuOBSD: version.h,v 1.27 2008/11/27 10:49:22 shadow Exp $ */
+/* $RuOBSD: version.h,v 1.28 2008/12/26 05:25:20 shadow Exp $ */
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
 #define VER_VER     0
 #define VER_SUBVER  9
-#define VER_REV     8
+#define VER_REV     9
 #define VER_SUBREV  0
 
 #endif /*__VERSION_H__*/
