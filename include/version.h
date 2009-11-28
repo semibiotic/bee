b2f79b5c2240eb5a475a9809eda6b76270e4706a
shadow <shadow@71f00a00-e368-4fde-a356-73d88c167424>
2009-11-28 10:40:57 +0000

    rev up (strict ip gates handling)
    git-svn-id: file:///svn/bee@378 71f00a00-e368-4fde-a356-73d88c167424

--- include/version.h
+++ include/version.h
@@ -1,11 +1,11 @@
-/* $RuOBSD: version.h,v 1.32 2009/04/10 06:47:22 shadow Exp $ */
+/* $RuOBSD: version.h,v 1.33 2009/05/14 17:02:06 shadow Exp $ */
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
 #define VER_VER     0
 #define VER_SUBVER  9
-#define VER_REV     12
-#define VER_SUBREV  1
+#define VER_REV     14
+#define VER_SUBREV  0
 
 #endif /*__VERSION_H__*/
 
